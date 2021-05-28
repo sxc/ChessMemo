953828631eeb4691910185ae78e166983cdb9be6
sxc <qiuyequ@icloud.com>
2021-05-29 07:23:31 +0800

    Initial Commit

---
+++ ChessMemo/ChessMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessMemoApp.swift
+//  ChessMemo
+//
+//  Created by Xiaochun Shen on 2021/5/29.
+//
+
+import SwiftUI
+
+@main
+struct ChessMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
