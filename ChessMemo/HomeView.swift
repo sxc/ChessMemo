8888b2a8ab42aa3414404d1beec45780b84ca02e
sxc <qiuyequ@icloud.com>
2021-05-29 13:56:06 +0800

    add Home view

---
+++ ChessMemo/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  ChessMemo
+//
+//  Created by Xiaochun Shen on 2021/5/29.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
