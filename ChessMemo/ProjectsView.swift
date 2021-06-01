7515333487a0c5b7af1fd2ca240a18478a5d84fd
sxc <qiuyequ@icloud.com>
2021-06-01 10:20:32 +0800

    storing tab selection

--- ChessMemo/ProjectsView.swift
+++ ChessMemo/ProjectsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ProjectsView: View {
+    static let openTag: String? = "Open"
+    static let closedTag: String? = "Close"
+    
     let showClosedProjects: Bool
     
     let projects: FetchRequest<Project>
