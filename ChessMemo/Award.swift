414af2ba901a43fe0bd02c04cd7e1b7f59dcdfb4
sxc <qiuyequ@icloud.com>
2021-06-03 14:42:54 +0800

    adding award to tab

---
+++ ChessMemo/Award.swift
@@ -0,0 +1,23 @@
+//
+//  Award.swift
+//  ChessMemo
+//
+//  Created by Xiaochun Shen on 2021/6/3.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+    
+}
