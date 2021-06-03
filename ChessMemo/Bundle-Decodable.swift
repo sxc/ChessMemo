dff3bf53bf5867b9ce23771c86898b86125b5249
sxc <qiuyequ@icloud.com>
2021-06-03 14:25:48 +0800

    add decode JSON

---
+++ ChessMemo/Bundle-Decodable.swift
@@ -0,0 +1,40 @@
+//
+//  Bundle-Decodable.swift
+//  ChessMemo
+//
+//  Created by Xiaochun Shen on 2021/6/3.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from file: String, dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) -> T {
+        // more code to come
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to located \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decorder = JSONDecoder()
+        decorder.dateDecodingStrategy = dateDecodingStrategy
+        decorder.keyDecodingStrategy = keyDecodingStrategy
+        
+        do {
+            return try decorder.decode(T.self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' - \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch - \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed to decode \(file) from bundle because it appears to be invalid JSON.")
+        } catch {
+            fatalError("Failed to decode \(file) from bundle: \(error.localizedDescription)")
+        }
+        
+    }
+}
