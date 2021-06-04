ec2981837cef8d3087082e795bd30b6789b2d3c2
sxc <qiuyequ@icloud.com>
2021-06-04 10:23:12 +0800

    add home list view

---
+++ ChessMemo/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  ChessMemo
+//
+//  Created by Xiaochun Shen on 2021/6/4.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
--- ChessMemo/HomeView.swift
+++ ChessMemo/HomeView.swift
@@ -5,6 +5,7 @@
 //  Created by Xiaochun Shen on 2021/5/29.
 //
 
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
@@ -12,19 +13,60 @@ struct HomeView: View {
     
     @EnvironmentObject var dataController: DataController
     
+    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+    
+    let items: FetchRequest<Item>
+    var projectRows: [GridItem] {
+        [GridItem(.fixed(100))]
+    }
+    
+    init() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+        
+        request.sortDescriptors = [ NSSortDescriptor(keyPath: \Item.priority, ascending: false)]
+        
+        request.fetchLimit = 10
+        
+        items = FetchRequest(fetchRequest: request)
+        
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                
-                Button("Add Data") {
-                    dataController.deleteAll()
-                    try? dataController.createSampleData()
+            ScrollView {
+                VStack (alignment: .leading) {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHGrid(rows: projectRows) {
+                            ForEach(projects) { project in
+                                VStack(alignment: .leading) {
+                                    Text("\(project.projectItems.count) items")
+                                        .font(.caption)
+                                        .foregroundColor(.secondary)
+                                    
+                                    Text(project.projectTitle)
+                                        .font(.title2)
+                                    
+                                    ProgressView(value: project.completionAmount)
+                                        .accentColor(Color(project.projectColor))
+                                }
+                            }
+                        }
+                    }
                 }
+                
             }
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
         }
     }
 }
+
+
+//Button("Add Data") {
+//    dataController.deleteAll()
+//    try? dataController.createSampleData()
+//}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
