340b7d653766547d0da9cec9d35c12f8a3665739
sxc <qiuyequ@icloud.com>
2021-06-15 10:04:43 +0800

    create home view

--- ChessMemo/HomeView.swift
+++ ChessMemo/HomeView.swift
@@ -13,21 +13,33 @@ struct HomeView: View {
     
     @EnvironmentObject var dataController: DataController
     
+//    @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+    
     @FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.title, ascending: true)], predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
     
+//    let items: FetchRequest<Item>
     let items: FetchRequest<Item>
     var projectRows: [GridItem] {
         [GridItem(.fixed(100))]
     }
     
+//    init() {
+//        let request: NSFetchRequest<Item> = Item.fetchRequest()
+//        request.predicate = NSPredicate(format: "completed = false")
+//
+        
     init() {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
         request.predicate = NSPredicate(format: "completed = false")
-        
-        request.sortDescriptors = [ NSSortDescriptor(keyPath: \Item.priority, ascending: false)]
+      
+
+        request.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Item.priority, ascending: false)
+        ]
         
         request.fetchLimit = 10
         
+       
         items = FetchRequest(fetchRequest: request)
         
     }
@@ -57,21 +69,103 @@ struct HomeView: View {
                             }
                         }
                         .padding([.horizontal, .top])
+                        .fixedSize(horizontal: false, vertical: true)
+                        
                     }
+                    
+                    VStack(alignment: .leading) {
+                        list("Up next", for: items.wrappedValue.prefix(3))
+                        list("More to explore", for: items.wrappedValue.dropFirst(3))
+                    }
+                    .padding(.horizontal)
                 }
-                
             }
             .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
         }
     }
-}
+    
+//    @ViewBuilder func list(_ title: String, for items: FetchedResults<Item>.SubSequence) -> some View {
+//
+////        if items.isEmpty {
+////            EmptyView()
+////        } else {
+//            Text(title)
+//                .font(.headline)
+//                .foregroundColor(.secondary)
+//                .padding(.top)
+//
+//            ForEach(items) { item in
+//                NavigationLink(destination: EditItemView(item: item)) {
+//                    HStack(spacing: 20) {
+//                        Circle()
+//                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
+//                            .frame(width: 44, height: 44)
+//
+//                        VStack(alignment: .leading) {
+//                            Text(item.itemTitle)
+//                                .font(.title2)
+//                                .foregroundColor(.primary)
+//                                .frame(maxWidth: .infinity, alignment: .leading)
+//
+//                            if item.itemDetail.isEmpty == false {
+//                                Text(item.itemDetail)
+//                                    .foregroundColor(.secondary)
+//                            }
+//                        }
+//                    }
+//                    .padding()
+//                    .background(Color.secondarySystemGroupedBackground)
+//                    .cornerRadius(10)
+//                    .shadow(color: Color.black.opacity(0.2), radius: 5)
+//                }
+//            }
+//        }
+    
+    @ViewBuilder func list(_ title: String, for items: FetchedResults<Item>.SubSequence) -> some View {
+        if items.isEmpty {
+            EmptyView()
+        } else {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.secondary)
+                .padding(.top)
+
+            ForEach(items) { item in
+                NavigationLink(destination: EditItemView(item: item)) {
+                    HStack(spacing: 20) {
+                        Circle()
+                            .stroke(Color(item.project?.projectColor ?? "Light Blue"), lineWidth: 3)
+                            .frame(width: 44, height: 44)
+
+                        VStack(alignment: .leading) {
+                            Text(item.itemTitle)
+                                .font(.title2)
+                                .foregroundColor(.primary)
+
+                            if item.itemDetail.isEmpty == false {
+                                Text(item.itemDetail)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    }
 
 
-//Button("Add Data") {
-//    dataController.deleteAll()
-//    try? dataController.createSampleData()
+//    Button("Add Data") {
+//        dataController.deleteAll()
+//        try? dataController.createSampleData()
+//    }
+
+    
 //}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
